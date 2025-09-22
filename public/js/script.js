e21d4e0196a41eebeefae5e9519e13e2cd97c238
dingemoe <108612432+dingemoe@users.noreply.github.com>
2025-09-22 16:29:41 +0000

    saddsf

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,18 @@
 // Sist redigert: 2025-09-22 / Fil: script.js
 console.log('Sist redigert: 2025-09-22 / Fil: script.js');
+
 import { el, els } from "./utils/dom.js";
 import { debounce } from "./utils/debounce.js";
 import { data, colWidths, currentRel, currentFilter, blankData, load, save, loadWidths, saveWidths } from "./data/data.js";
 import { updateRelDataList, handleUrlParameter, hideContextMenu } from "./ui/ui.js";
-// ... Lim inn resten av script.js-koden din her, med alle imports oppdatert til relative stier ...
+
+(async () => {
+	colWidths = await loadWidths();
+	data = await load();
+	if (!data.headers || !Array.isArray(data.headers) || data.headers.length === 0) {
+		data.headers = ["done", "Kolonne 1", "Kolonne 2", "Kolonne 3", "Rel"];
+	}
+	handleUrlParameter();
+	initEvents();
+	render();
+})();
